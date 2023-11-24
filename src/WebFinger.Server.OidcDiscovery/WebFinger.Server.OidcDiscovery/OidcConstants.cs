1b21dd9ca2dac831882b319df80402a5eebdd741
David Retzlaff <david.retzlaff@bluehands.de>
2023-11-24 12:09:24 +0100

    add source

---
+++ src/WebFinger.Server.OidcDiscovery/WebFinger.Server.OidcDiscovery/OidcConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace WebFinger.Server.OidcDiscovery;
+
+public static class OidcConstants
+{
+    // ReSharper disable once InconsistentNaming
+    public const string OPENID_ISSUER_RELATION = "http://openid.net/specs/connect/1.0/issuer";
+}
---
+++ src/WebFinger.Server.OidcDiscovery/WebFinger.Server.OidcDiscovery/WebFingerOidcExtension.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Immutable;
+using DarkLink.Web.WebFinger.Server;
+using DarkLink.Web.WebFinger.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebFinger.Server.OidcDiscovery;
+
+// ReSharper disable once UnusedType.Global
+public static class WebFingerOidcExtension
+{
+    // ReSharper disable once UnusedMember.Global
+    public static IServiceCollection AddOidcWebFinger(this IServiceCollection services, OidcIssuer oidcIssuer)
+    {
+        services.AddWebFinger(new OidcResourceDescriptorProvider(oidcIssuer));
+        return services;
+    }
+
+    private class OidcResourceDescriptorProvider : IResourceDescriptorProvider
+    {
+        private readonly OidcIssuer issuer;
+
+        public OidcResourceDescriptorProvider(OidcIssuer issuer)
+        {
+            this.issuer = issuer;
+        }
+
+        public Task<JsonResourceDescriptor?> GetResourceDescriptorAsync(
+            Uri resource,
+            IReadOnlyList<string> relations,
+            HttpRequest request,
+            CancellationToken cancellationToken) => Task.FromResult<JsonResourceDescriptor?>(new JsonResourceDescriptor(
+            resource,
+            ImmutableList<Uri>.Empty,
+            ImmutableDictionary<Uri, string?>.Empty,
+            relations.Count == 0 || relations.Contains(OidcConstants.OPENID_ISSUER_RELATION)
+                ? ImmutableList.Create(new Link(
+                    OidcConstants.OPENID_ISSUER_RELATION,
+                    default,
+                    issuer.Endpoint,
+                    ImmutableDictionary<string, string>.Empty,
+                    ImmutableDictionary<Uri, string?>.Empty))
+                : ImmutableList<Link>.Empty
+        ));
+    }
+}
